f1b6b6b5c4239416705991d64b12279851283d0b
Frank Leng <email@frankleng.me>
2020-05-10 16:03:05 -0700

    add ci, bump version 1.0.4 (+3 squashed commits) Squashed commits: [a4911a0] ignore /lib [428e272] remove /lib [97d4b79] 1.0.3
    workflows version 2
    
    fix CI workflow

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function getActionConstants(constant: string): string[];
-export declare function getAsyncDispatch(store: {
-    dispatch: any;
-}): (promise: Promise<any>, [initAction, successAction, failureAction]: string[], path?: null) => Promise<any>;
-export default getAsyncDispatch;
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,24 @@ function getDispatchable(action: {} | string) {
   return typeof action === 'string' ? { type: action } : { ...action };
 }
 
+export function initCase([initAction]: string[]) {
+  return initAction;
+}
+
+export function successCase([, successAction]: string[]) {
+  return successAction;
+}
+
+export function failureCase([, , failureAction]: string[]) {
+  return failureAction;
+}
+
 export function getActionConstants(constant: string) {
   return [constant, `${constant}_SUCCESS`, `${constant}_FAILURE`];
 }
 
 export function getAsyncDispatch(store: { dispatch: any }) {
-  return (promise: Promise<any>, [initAction, successAction, failureAction]: string[], path = null) => {
+  return (promise: Promise<any>, [initAction, successAction, failureAction]: string[]) => {
     const { dispatch } = store;
 
     if (initAction) dispatch(getDispatchable(initAction));
