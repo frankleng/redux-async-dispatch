900695650a6735704b6eb4f75d447dad0d877e20
Frank Leng <email@frankleng.me>
2020-05-15 16:47:27 -0700

    target es6, added match helpers, docs

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # redux-dispatch-promise
 
-docs coming soon
+### Setup
+
+```js
+// redux store - apply redux-thunk middleware
+import thunk from 'redux-thunk';
+import { matchAnyFailure, getAsyncDispatch } from 'redux-dispatch-promise';
+
+const appReducer = combineReducers({ account });
+const rootReducer = (state: any, action: any) => {
+  //root actions goes here like app-wide logoff
+  // use match helper to match any 401 error to trigger logoff
+  if (action.type === LOG_OFF || matchAnyFailure(action, (data) => data.status === 401)) {
+    // setting app state to undefined forces redux to load initial state for all reducers
+    state = undefined;
+  }
+
+  return appReducer(state, action);
+};
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+export default store;
+
+export const dispatchAsync = getAsyncDispatch(store);
+
+// action constants
+export const ASYNC_LOGIN = getActionConstants('ACCOUNT/LOGIN');
+// produces ACCOUNT_LOGIN, ACCOUNT_LOGIN_SUCCESS, ACCOUNT_LOGIN_FAILURE
+
+// reducer
+export default function accountReducer(state, action) {
+  switch (action.type) {
+    case getSuccessAction(ASYNC_LOGIN):
+    // update state here
+    default:
+      return state;
+  }
+}
+
+// action creator
+export async function callApi() {
+  const promise = await asyncCall();
+  return dispatchAsync(promise, ASYNC_LOGIN);
+}
+```
+
+### Changelog
+
+v 1.1.0 - added helpers to match any successful/failed actions. useful for handling actions with app-wide effects - ex. 401 error from a route that should cause entire store to reset.
+
+---
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,39 @@
-function getDispatchable(action: {} | string) {
+type DispatchableAction = {
+  type: string;
+  data?: any;
+};
+
+const successSuffix = '_SUCCESS';
+const failureSuffix = '_FAILURE';
+
+function getDispatchable(action: DispatchableAction | string): DispatchableAction {
   return typeof action === 'string' ? { type: action } : { ...action };
 }
 
-export function initCase([initAction]: string[]) {
+export function getInitAction([initAction]: string[]) {
   return initAction;
 }
 
-export function successCase([, successAction]: string[]) {
+export function getSuccessAction([, successAction]: string[]) {
   return successAction;
 }
 
-export function failureCase([, , failureAction]: string[]) {
+export function getFailureAction([, , failureAction]: string[]) {
   return failureAction;
 }
 
+export function matchAnySuccessAction(action: DispatchableAction, matcher?: (data: any) => boolean): boolean {
+  const isSuccess = action.type?.endsWith(successSuffix);
+  return matcher && action.data ? isSuccess && matcher(action.data) : isSuccess;
+}
+
+export function matchAnyFailureAction(action: DispatchableAction, matcher?: (data: any) => boolean): boolean {
+  const isFailure = action.type?.endsWith(failureSuffix);
+  return matcher && action.data ? isFailure && matcher(action.data) : isFailure;
+}
+
 export function getActionConstants(constant: string) {
-  return [constant, `${constant}_SUCCESS`, `${constant}_FAILURE`];
+  return [constant, `${constant}${successSuffix}`, `${constant}${failureSuffix}`];
 }
 
 export function getAsyncDispatch(store: { dispatch: any }) {
@@ -33,7 +51,7 @@ export function getAsyncDispatch(store: { dispatch: any }) {
       })
       .catch((err) => {
         if (failureAction) {
-          dispatch({ ...getDispatchable(failureAction) });
+          dispatch({ ...getDispatchable(failureAction), data: err });
           return err;
         }
       });
